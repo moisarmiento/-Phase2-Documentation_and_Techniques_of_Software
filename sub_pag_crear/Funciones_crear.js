f39350c3e51326f26b8ba965eb27985b577416dd
moisarmiento <davmartinez2@gmail.com>
2024-10-12 16:08:24 -0500

    se agrego mensaje de ok a crear

--- sub_pag_crear/Funciones_crear.js
+++ sub_pag_crear/Funciones_crear.js
@@ -10,12 +10,17 @@ document.getElementById("productForm").addEventListener("submit", function(event
     .then(response => response.text())
     .then(result => {
         const messageDiv = document.getElementById("message");
-        if (result.includes("Exito")) { // 
+        if (result.includes("Exito")) {
             messageDiv.textContent = "La información fue enviada correctamente.";
-            messageDiv.style.color = "green"; //
+            messageDiv.style.color = "green"; 
+            // Mostrar alerta de confirmación
+            if (confirm("La información fue enviada correctamente. ¿Desea continuar a la página de actualización?")) {
+                // Redirigir a la página de actualización si el usuario acepta
+                cargar_actualizar_si();
+            }
         } else {
             messageDiv.textContent = "Hubo un problema al enviar la información.";
-            messageDiv.style.color = "red"; // 
+            messageDiv.style.color = "red"; 
         }
         console.log(result); // Muestra la respuesta en la consola
     })
@@ -23,6 +28,7 @@ document.getElementById("productForm").addEventListener("submit", function(event
         console.error('Error:', error);
     });
 });
+
 function cargar_actualizar_si() {
     window.location.href = 'http://localhost/fase-2-unad/-Phase2-Documentation_and_Techniques_of_Software/sub_pag_actualizar/sub_pagina_Actualizar.html';
 }
